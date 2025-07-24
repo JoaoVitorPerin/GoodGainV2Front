5afa0f768691297edcd0065cf92805ba3209ca09
Joao Vitor Perin <joao.perin@inteliger.com.br>
2025-07-24 09:54:59 -0300

    feat(layout): :sparkles: ajustes no layout principal

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -5,11 +5,19 @@ import { TokenService } from '../shared/services/token.service';
 
 @Component({
     selector: 'app-menu',
-    templateUrl: './app.menu.component.html'
+    templateUrl: './app.menu.component.html',
+    styleUrls: ['./app.menu.component.css'],
 })
 export class AppMenuComponent implements OnInit {
 
-    model: any[] = [];
+    model: any[] = [
+        { label: 'Teste 1', icon: 'pi pi-fw pi-home', routerLink: ['/'] },
+        { label: 'Teste 2', icon: 'pi pi-fw pi-calendar', routerLink: ['/teste2'] },
+        { label: 'Teste 3', icon: 'pi pi-fw pi-users', routerLink: ['/teste3'] },
+        { label: 'Teste 4', icon: 'pi pi-fw pi-cog', routerLink: ['/teste4'] },
+        { label: 'Teste 5', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/teste5'] },
+        { label: 'Teste 6', icon: 'pi pi-fw pi-file', routerLink: ['/teste6'] }
+    ];
 
     constructor(
         public layoutService: LayoutService,
@@ -18,68 +26,5 @@ export class AppMenuComponent implements OnInit {
 
     ngOnInit() {
         const permissoes = this.tokenService.getPermissions() || [];
-        
-        if(permissoes.includes('Administrador')) {
-            this.model.push(
-                {
-                    label: 'Dashboard',
-                    items: [
-                        { label: 'Estoque', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/dashboard/estoque'] },
-                        { label: 'Vendas e custos', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/dashboard/vendas-custos'] },
-                        { label: 'Vendas por marcas', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/dashboard/vendas-marcas'] },
-                        { label: 'Atletas', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/dashboard/custo-atleta'] },
-                    ]
-                }
-            )
-        }
-
-        if(permissoes.includes('Operador de Estoque') || permissoes.includes('Administrador') || permissoes.includes('Operador de Pedidos')) {
-            this.model.push(
-                {
-                    label: 'Gestão de Pedidos',
-                    items: [
-                        { label: 'Pedidos', icon: 'pi pi-fw pi-shopping-cart', routerLink: ['/pedido/home'] },
-                        { label: 'Clientes', icon: 'pi pi-fw pi-users', routerLink: ['/cliente/home'] },
-                        { label: 'Separação', icon: 'pi pi-fw pi-truck', routerLink: ['/separar-pedido'] },
-                    ]
-                }
-            )
-        }
-
-        if(permissoes.includes('Administrador')) {
-            this.model.push(
-                {
-                    label: 'Estoque',
-                    items: [
-                        { label: 'Movimentações', icon: 'pi pi-fw pi-arrows-v', routerLink: ['/movimentacao-estoque'] },
-                    ]
-                }
-            )
-        }
-
-        if(permissoes.includes('Administrador')) {
-            this.model.push(
-                {
-                    label: 'Gestão Produto',
-                    items: [
-                        { label: 'Produtos e Estoque', icon: 'pi pi-fw pi-box', routerLink: ['/produto/home'] },
-                        { label: 'Marcas', icon: 'pi pi-fw pi-bookmark', routerLink: ['/marca/home'] },
-                        { label: 'Categorias', icon: 'pi pi-fw pi-list', routerLink: ['/categoria/home'] },
-                    ]
-                }
-            )
-        }
-
-        if(permissoes.includes('Administrador')) {
-            this.model.push(
-                {
-                    label: 'Gestão Admin',
-                    items: [
-                        { label: 'Usuários', icon: 'pi pi-fw pi-user', routerLink: ['/user/home'] },
-                        { label: 'Custo mensal', icon: 'pi pi-fw pi-dollar', routerLink: ['/custo-mensal/home'] }
-                    ]
-                },
-            )
-        }
     }
 }
